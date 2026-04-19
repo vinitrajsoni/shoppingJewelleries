d78a9c3e667c69274f0eb33b44f2be4eabd4017c
Vinit Raj Soni <vinitrajsoni52@gmail.com>
2026-04-19 12:10:44 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://shoppingjewelleries.onrender.com/api";
 
 export const getProducts = async () => {
   const res = await fetch(`${BASE_URL}/products`);
